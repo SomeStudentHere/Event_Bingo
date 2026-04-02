6b14cd17b1cecc713dd600984f6d89b80fd80f3e
SomeStudentHere <2222403@my.ipleiria.pt>
2026-04-02 19:23:22 +0100

    Initial Comit

---
+++ src/main/java/pt/IPLeiria/event_bingo/EventBingoApplication.java
@@ -0,0 +1,13 @@
+package pt.IPLeiria.event_bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/IPLeiria/event_bingo/EventBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.IPLeiria.event_bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
